e8899b2480aab3294db25df9f6057f8f4414b097
Karl Belousov <belousovkarl@gmail.com>
2024-01-21 21:48:18 +0600

    вёрстка главной страницы

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const openButtonCart = document.querySelector(".user-menu__button--open-cart");
+const cart = document.querySelector(".cart");
+const openSubmenu = document.querySelector(".menu-list__button");
+const submenu = document.querySelector(".submenu");
+const tabsItems = document.querySelectorAll(".services__tabs-item");
+const servicesItems = document.querySelectorAll(".services-item");
+
+openButtonCart.addEventListener("click", () => {
+  cart.classList.toggle("hidden");
+});
+
+openSubmenu.addEventListener("click", () => {
+  openSubmenu.classList.toggle("open");
+  submenu.classList.toggle("submenu--open");
+});
+
+tabsItems.forEach((tabItem) => {
+  tabItem.addEventListener("click", (evt) => {
+    console.log("Click!");
+    tabItem.classList.remove("active");
+    evt.target.classList.add("active");
+  });
+});
