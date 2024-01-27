69e9fa65a088a5f9345d4e4c81b48677863fcad5
Karl Belousov <belousovkarl@gmail.com>
2024-01-27 17:51:02 +0600

    вёрстка модального окна

--- js/script.js
+++ js/script.js
@@ -3,10 +3,13 @@ const cart = document.querySelector(".cart");
 const openSubmenu = document.querySelector(".menu-list__button");
 const submenu = document.querySelector(".submenu");
 const tabsItems = document.querySelectorAll(".services__tabs-item");
-const delivery = document.querySelector(".delivery");
+const main = document.querySelector(".main");
+const delivery = main.querySelector(".delivery");
 const modalContainer = document.querySelector(".modal-overlay");
-const modal = modalContainer.querySelector(".modal");
-const modalcloseButton = modal.querySelector(".modal__close-button");
+const modal = document.querySelector(".modal");
+const modalcloseButton = document.querySelector(".modal__close-button");
+const servicesTabs = main.querySelectorAll(".services__tabs-item");
+const servicesItems = main.querySelectorAll(".services__item");
 
 openButtonCart.addEventListener("click", () => {
   cart.classList.toggle("hidden");
@@ -16,6 +19,23 @@ openSubmenu.addEventListener("click", (evt) => {
   evt.preventDefault();
   openSubmenu.classList.toggle("open");
   submenu.classList.toggle("submenu--open");
+});
+
+servicesTabs.forEach((servicesTab) => {
+  const tabBtn = servicesTab.querySelector(".button");
+  tabBtn.addEventListener("click", () => {
+    let currentTub = main.querySelector(".services__tabs-item.active");
+    currentTub.classList.remove("active");
+    servicesTab.classList.add("active");
+
+    servicesItems.forEach((item) => {
+      if (tabBtn.dataset.tub == item.dataset.value) {
+        item.classList.remove("hidden");
+      } else {
+        item.classList.add("hidden");
+      }
+    });
+  });
 });
 
 function onEscKeyDown(evt) {
